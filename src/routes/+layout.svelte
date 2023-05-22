be8294e985a97685d897c338d3561ad97d010609
Olga <kovalolya2008@icloud.com>
2023-05-22 17:12:18 +0300

    Загрузка сайта

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Андрей Кузнецов</a>
+        <div class="nav-links">
+            {#each nav as link}
+				{#if link.isShow}
+				<a href={link.href} class="link">{link.title}</a>
+				{/if}
+            {/each}
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!-- Pages will be injected below -->
+    <slot></slot>
+</div>
+<script>
+    	const nav = [
+		{
+			title: 'Обо мне',
+			href: '/about',
+			isShow: true
+		},
+		{
+			title: 'Блог',
+			href: '/blog',
+			isShow: true
+		},
+		{
+			title: 'Портфолио',
+			href: '/project',
+			isShow: true
+		},
+		{
+			title: 'Контакты',
+			href: '/contact',
+			isShow: true
+		},
+        {
+			title: 'temp',
+			href: '/temp',
+			isShow: false
+		}
+	]
+</script>
+<style>
+       .container {
+           margin: 50px auto;
+           max-width: 800px;
+           padding: 0 20px;
+       }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,93 @@
+<svelte:head>
+    <title>Имя Фамилия</title>
+</svelte:head>
+
+<div class="container">
+    <div class="info">
+        <div class="animate-background">
+            <div class="container info-container">
+                <div class="info-text">
+                    <h1>
+                    <p class="animate-text-1">Персональный сайт,</p>
+                    <p class="text-2">Андрея Кузнецова</p>
+                    </h1>
+                </div>
+                <h2 class="subtitle">
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto aut, cupiditate dolores ducimus expedita labore nemo odio quibusdam quidem sapiente soluta.
+                </h2>
+                <div style="color:black" class="scroll-button animate-scroll" tabindex="0">
+                    <a href="/about">Узнать обо мне</a><div class="arrow">→</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="background-div"></div>
+
+
+    <style>
+    .info {
+        text-align: center;
+        position: relative;
+    }
+
+    .info-container {
+        height: 100%;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+
+    .container {
+        max-width: 1400px;
+        margin: 190px 0 0 2.5rem;
+    }
+
+    .info h1 {
+        font-size: 3rem;
+        margin: 0;
+    }
+
+    .info h1 p {
+        position: relative;
+        font: inherit;
+        margin: 0;
+        text-align: left;
+        line-height: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+
+    .info h2 {
+        max-width: 65ch;
+        font-weight: 200;
+        margin-top: 0.50rem;
+    }
+
+    .background-div {
+        background-color: #ecebe8;
+        width: 100%;
+        height: 56vh;
+        margin: -205px auto;
+    }
+
+
+    .info .info-text {
+        position: relative;
+    }
+
+    .scroll-button {
+        display: flex;
+        border: none;
+        outline: none;
+        cursor: pointer;
+    }
+
+    .scroll-button .arrow {
+        margin-left: 0.50rem;
+    }
+</style>
